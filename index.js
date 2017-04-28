8ffff2805c1153521882d922c0d3f6490e03f9bb
EQuimper <quimperemanuel@gmail.com>
2017-04-27 22:43:43 -0400

    Create server for deploy

---
+++ index.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-console */
+const { createServer } = require('http');
+const next = require('next');
+
+const app = next();
+const handle = app.getRequestHandler()
+
+app.prepare()
+  .then(() => {
+    createServer((req, res ) => {
+      handle(req, res);
+    })
+    .listen(3000, (err) => {
+      if (err) throw err
+      console.log('> Ready on http://localhost:3000')
+    })
+  })
